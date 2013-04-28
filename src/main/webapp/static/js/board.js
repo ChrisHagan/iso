abe08162cce8051ed109add81420f5bf4ce94d8e
Chris Hagan <captain.mister.hagan@gmail.com>
2013-04-28 10:49:16 +1000

    Got jump trig calculations correct

--- src/main/webapp/static/js/board.js
+++ src/main/webapp/static/js/board.js
@@ -21,21 +21,28 @@ var graph = {
     players:{},
     rings:{
         0:{
+            id:0,
             radius:200,
             x:0,
-            y:0
+            y:0,
+            links:{}
         }
     }
 };
 function addRing(radius,parentId,arcExtent){
-    var offset = graph.rings[parentId].radius + radius;
-    graph.rings[_.keys(graph.rings).length] = {
+    var parent = graph.rings[parentId];
+    var offset = parent.radius + radius;
+    var id = _.keys(graph.rings).length;
+    graph.rings[id] = {
+        id:id,
+        links:{},
         radius:radius,
-        arcExtent:arcExtent,
-        parent:parentId,
         x:offset * Math.cos(arcExtent),
         y:offset * Math.sin(arcExtent)
     };
+    var reflectedExtent = clampToCircle(arcExtent + degreesToRadians(180));
+    graph.rings[id].links[parentId] = reflectedExtent;
+    parent.links[id] = arcExtent;
 }
 var board;
 var view = {};
@@ -49,7 +56,9 @@ function join(name,ring,arcExtent){
         arcExtent:arcExtent,
         width:100,
         height:36,
-        img:resources.boat
+        jumping:false,
+        img:resources.boat,
+        speed:degreesToRadians(2)
     };
 }
 function localToWorld(d){
@@ -63,6 +72,21 @@ function drawRing(c){
         var worldC = localToWorld(r);
         var p = resources.planet;
         var side = r.radius / 5;
+        c.strokeStyle = style.orbit;
+        $.each(r.links,function(k,v){
+            c.beginPath();
+            c.arc(worldC.x,
+                  worldC.y,
+                  r.radius - 20,
+                  v-0.1,
+                  v+0.1);
+            c.stroke();
+        });
+        c.fillStyle = "white";
+        c.font = "bold 16px Arial";
+        c.fillText(""+r.id,
+                   worldC.x,
+                   worldC.y - 30);
         c.drawImage(p,
                     worldC.x - side / 2,
                     worldC.y - side / 2,
@@ -70,53 +94,66 @@ function drawRing(c){
                     side);
     };
 }
+function ringChecker(player){
+    var threshold = 20;
+    return function(ring){
+        return isAttainableRing(player,ring,threshold);
+    };
+}
+function isConnected(r1,r2){
+    return r2.id in r1.links;
+}
+function playerLocalCoords(player,pRing){
+    return {
+	x:pRing.x + pRing.radius * Math.cos(player.arcExtent),
+	y:pRing.y + pRing.radius * Math.sin(player.arcExtent)
+    };
+}
+function playerToRingDistance(player,pRing,ring){
+    var playerCoords = playerLocalCoords(player,pRing);
+    var xDelta = playerCoords.x - ring.x;
+    var yDelta = playerCoords.y - ring.y;
+    return Math.sqrt(Math.pow(xDelta,2) + Math.pow(yDelta,2));
+}
+function isAttainableRing(player,ring,threshold){
+    var pRing = graph.rings[player.ring];
+    var alternative = ring.id != player.ring;
+    var contiguous = isConnected(pRing,ring);
+    var distanceToRing = playerToRingDistance(player,pRing,ring);
+    var close =  distanceToRing < ring.radius + threshold;
+    var attainable = alternative && contiguous && close;
+    return attainable;
+}
 function drawAttainableRing(c,p){
-    var threshold = degreesToRadians(30);
-    return function(i,r){
-        if(r.parent == p.ring){
-            if(Math.abs(p.arcExtent - r.arcExtent) < threshold){
-                var attainableWorldCoords = localToWorld(r);
-                var reflectionOffset = degreesToRadians(180);
-                c.beginPath();
-                c.arc(
-                    attainableWorldCoords.x,
-                    attainableWorldCoords.y,
-                    r.radius,
-                    r.arcExtent + reflectionOffset,
-                    r.arcExtent - degreesToRadians(50) + reflectionOffset,
-                    true);
-                c.stroke();
-            }
-        };
-    }
+    var threshold = 100;
+    return function(id){
+        var r = graph.rings[id];
+        if(isAttainableRing(p,r,threshold)){
+            var attainableWorldCoords = localToWorld(r);
+            var gradientToPlayerRing = r.links[p.ring];
+            c.strokeStyle = style.orbit;
+            c.beginPath();
+            c.arc(
+                attainableWorldCoords.x,
+                attainableWorldCoords.y,
+                r.radius,
+                gradientToPlayerRing,
+                gradientToPlayerRing - threshold);
+            c.stroke();
+        }
+    };
 }
 var style = {
     orbit:"red"
 };
 function drawPlayer(c){
     return function(i,p){
-        var r = graph.rings[p.ring];
-        var local = {
-            x:r.radius * Math.cos(p.arcExtent),
-            y:r.radius * Math.sin(p.arcExtent)
-        };
+        var pRing = graph.rings[p.ring];
+        var local = playerLocalCoords(p,pRing);
         var playerWorldCoords = localToWorld(local);
-        var focus = graph.rings[p.ring];
-        var focusWorldCoords = localToWorld(focus);
+        var focusWorldCoords = localToWorld(pRing);
 
-        var orbitExtent = degreesToRadians(30);
-        c.strokeStyle = style.orbit;
-        c.beginPath();
-        c.arc(
-            focusWorldCoords.x,
-            focusWorldCoords.y,
-            r.radius,
-            p.arcExtent,
-            p.arcExtent + orbitExtent
-        );
-        c.stroke();
-
-        $.each(graph.rings,drawAttainableRing(c,p));
+        $.each(pRing.links,drawAttainableRing(c,p));
 
         c.drawImage(p.img,
                     playerWorldCoords.x - p.width / 2,
@@ -125,8 +162,22 @@ function drawPlayer(c){
                     p.height);
     }
 }
+function clampToCircle(radians){
+    return radians % degreesToRadians(360);
+}
 function tickPlayer(p){
-    p.arcExtent = (p.arcExtent += 0.05) % degreesToRadians(360);
+    if(p.jumping){
+        var attainableRings = _.values(graph.rings).filter(ringChecker(p));
+        if(attainableRings.length > 0){
+            var pRing = graph.rings[p.ring];
+            var newRing = attainableRings[0];
+            p.speed *= -1;
+            p.arcExtent = newRing.links[p.ring];
+            p.ring = newRing.id;
+            p.jumping = false;
+        }
+    }
+    p.arcExtent = clampToCircle(p.arcExtent + p.speed);
 }
 function draw(b){
     requestAnimationFrame(draw);
@@ -159,10 +210,9 @@ resources.ready = function(){
     view.y = board.height() / -2;
     view.width = board.width();
     view.height = board.height();
-    addRing(200,0,1);
-    addRing(100,0,4);
-    addRing(80,0,2);
-    addRing(200,0,5);
+    for(var i = 0; i < 4; i++){
+        addRing(100,0,i);
+    }
     join("player",0,Math.PI);
     requestAnimationFrame(draw);
 };
@@ -171,5 +221,12 @@ $(function(){
     resources.pending = toReady.length;
     $.each(toReady,function(i,r){
         readyResource(r);
+    });
+    $("body").on("keypress",function(k){
+        switch(k.which){
+        case 32://space
+            graph.players.player.jumping = true;
+            break;
+        }
     });
 });
